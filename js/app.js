81571083fac99e9061f448ff03afc5050caa6c9b
Alex Kaminski <alexdkaminski@gmail.com>
2019-11-03 18:32:43 +1000

    Add card matching functionality

--- js/app.js
+++ js/app.js
@@ -1,51 +1,52 @@
 /*
  * Create a list that holds all of your cards
  */
-let cards = ['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bicycle','fa-bomb']
+let cards = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb', 'diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb']
 
 const deck = document.getElementsByClassName('deck')[0];
+
+let openCards = [];
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
 displayCards(cards);
 
 function displayCards(cards) {
-    shuffle(cards);
-    cards.forEach(card => {
-        let i = document.createElement('i');
-        i.classList = `fa ${card}`;
-        let li = document.createElement('li');
-        li.className = 'card';
-        li.appendChild(i);
-        deck.appendChild(li);
-    })
-    console.log('Cards displayed')
+	// shuffle(cards);
+	cards.forEach(card => {
+		let i = document.createElement('i');
+		i.classList = `fa fa-${card}`;
+		let li = document.createElement('li');
+		li.className = 'card';
+		li.appendChild(i);
+		deck.appendChild(li);
+	})
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    return array;
+	return array;
 }
 
 
 /*
  * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  [x] display the card's symbol (put this functionality in another function that you call from this one)
+ *  [x] add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
@@ -53,12 +54,44 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-deck.addEventListener('click', function(e) {
-    let card = e.target;
-    card.classList('card open show')
+deck.addEventListener('click', function (e) {
+	let card = e.target;
+	displayCard(card);
+	checkForMatch(card);
 })
 
-function clickHandler(e){
-    let card = e.target
-    console.log(card);
+function displayCard(card) {
+	// display the card's symbol
+	card.classList.add('open', 'show');
+}
+
+function checkForMatch(card) {
+	// add the card to a *list* of "open" cards
+	openCards.push(card);
+
+	if (openCards.length == 2) {
+		if (openCards[0].children[0].classList[1].substr(3) == openCards[1].children[0].classList[1].substr(3)) {
+			console.log("Match")
+			console.log(openCards);
+			cardMatch();
+		} else {
+			setTimeout(cardNoMatch,1000);
+		}
+	}
+	console.log(openCards);
+}
+
+function cardMatch() {
+	openCards.forEach((card)=> {
+		card.classList.remove('open', 'show');
+		card.classList.add('match');
+		openCards = [];
+	})
+}
+
+function cardNoMatch() {
+	openCards.forEach((card)=> {
+		card.classList.remove('open', 'show');
+		openCards = [];
+	})
 }
